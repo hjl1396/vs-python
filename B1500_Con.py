a17599da60f16910c756a636a6970553ecb6492e
hjl1396 <hjl1396@gmail.com>
2020-04-08 20:56:26 +0800

    1500

--- B1500_Con.py
+++ B1500_Con.py
@@ -30,7 +30,21 @@ esio2=3.9
 esic=9.7
 e0=8.85e-12 #(F/m)
 q=1.6e-19
+
 #------------------输入--------------------
-Save_file='C:\Users\haojilong\Desktop\O1\RX\Dit.xlsx'
+Save_file= 'C:/Users/hjl13/Desktop/O1/RX/Dit.xlsx'
+print("请输入参数（用空格隔开）")
+print("温度（K）,氧化层电容（pF）,串联电阻（Ω）,串联电感（uH）,测试半径（um）,请输入RX文件所在位置,        掺杂浓度（cm-3）")
+print(r'350          46.293          24.0348       7.703         164.93  C:\Users\haojilong\Desktop\O1\RX    7.5214e15,JL1 ')
+print("(若对应参数无需修改输入0)")
 
+inp_data1=[350,46.293,24.0348,7.703,164.93,r'C:\Users\haojilong\Desktop\O1\RX',7.5214e15,r'JL1']
 
+inp_data = list(map(input().split()))
+
+for i in range(0, len(inp_data)):
+    if(inp_data[i]!=0):
+        inp_data1[i]=inp_data[i]
+
+for i in range(0, len(inp_data1)):
+    print(inp_data1[i], "\n", end="")
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-nums = list(map(int, input().split()))
+nums = list(map(float, input().split()))
 for j in range(0, len(nums)):
-    print(nums[j], "\n", end="") 
+    print(nums[j], "\n", end="")
 print("写入数据成功！")
